c87c1bf0038b6a5d873f57c48344f315d962d210
Kuryu86 <Kuryu86+github@gmail.com>
2020-01-05 12:09:49 +0900

    ノード設定関数の追加

--- main.c
+++ main.c
@@ -1,12 +1,53 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+typedef struct _TreeNode
+{
+	struct _TreeNode *left;
+	struct _TreeNode *right;
+	int data;
+}TreeNode;
+void SetTree(TreeNode *Node,int data)
+{
+	if(Node->data>data)
+	{
+		if(Node->left==NULL)
+		{
+			Node->left=(TreeNode *)malloc(sizeof(TreeNode));
+			Node->left->data = data;
+		}
+		else
+		{
+			SetTree(Node->left, data);
+		}
+	}
+	else
+	{
+		if(Node->right==NULL)
+		{
+			Node->right=(TreeNode *)malloc(sizeof(TreeNode));
+			Node->right->data = data;
+		}
+		else
+		{
+			SetTree(Node->right, data);
+		}
+	}
+	
+}
 int main(int argc, char *argv[]) 
 {
     char str[7];
-    
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
-    }
+	TreeNode *Tree;
+	while (fgets(str, sizeof(str), stdin))
+	{
+		printf("%s", str);
+		int input=atoi(str);
+		if(Tree==NULL)
+		{
+			Tree = (TreeNode *)malloc(sizeof(TreeNode));
+			Tree->data = input;
+		}
+	}
 
-    return 0;
+	return 0;
 }
